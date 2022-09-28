17f53f2ea67137207489116179981d7afc872410
tysonOliveira <thaisonoliveira@gmail.com>
2022-09-28 01:35:11 -0300

    created taskInfo

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -1,3 +1,5 @@
+import { PlusCircle } from 'phosphor-react';
+
 import style from './AddTask.module.css';
 
 export function AddTask() {
@@ -5,9 +7,14 @@ export function AddTask() {
     <div >
       <section className={style.task}>
         <input className={style.taskName} type="text" placeholder="Adicione uma nova tarefa" />
-        <button className={style.createTaskButton}>Criar</button>
+        <button className={style.createTaskButton}>Criar <PlusCircle size={20} weight="bold"/></button>
       </section>
+
       <section>
+        <header>
+          <span>Tarefas criadas</span>
+          <span>Concluídas</span>
+        </header>
 
       </section>
     </div>
