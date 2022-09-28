7f3502ca4c486fb907364e36c8619a4daf24e622
tysonOliveira <thaisonoliveira@gmail.com>
2022-09-28 02:15:50 -0300

    css task

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -4,16 +4,16 @@ import style from './AddTask.module.css';
 
 export function AddTask() {
   return (
-    <div >
+    <div>
       <section className={style.task}>
         <input className={style.taskName} type="text" placeholder="Adicione uma nova tarefa" />
         <button className={style.createTaskButton}>Criar <PlusCircle size={20} weight="bold"/></button>
       </section>
 
-      <section>
-        <header>
-          <span>Tarefas criadas</span>
-          <span>Concluídas</span>
+      <section className={style.taskInfo}>
+        <header className={style.taskInfoHeader}>
+          <span className={style.createdTask}>Tarefas criadas <span className={style.info}>0</span></span>
+          <span>Concluídas <span className={style.info}>0</span></span>
         </header>
 
       </section>
