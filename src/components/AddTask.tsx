9c6de62a1251b8958467f4d757b23cf3d5b06fc2
tysonOliveira <thaisonoliveira@gmail.com>
2022-10-01 00:29:21 -0300

    state

--- src/components/AddTask.tsx
+++ src/components/AddTask.tsx
@@ -1,4 +1,5 @@
 import { PlusCircle } from 'phosphor-react';
+import { ChangeEvent, FormEvent, useState } from 'react';
 
 import style from './AddTask.module.css';
 
@@ -6,35 +7,63 @@ import { TaskArea } from './TaskArea';
 import { TaskCard } from './TaskCard';
 
 export function AddTask() {
-  const taskNumbers = 0;
-  const sum = [{
-    text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia enim doloremque omnis totam soluta voluptates eligendi quos amet quam rerum',
-  },{
-    text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia enim doloremque omnis totam soluta voluptates eligendi quos amet quam rerum'
-  },{
-    text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia enim doloremque omnis totam soluta voluptates eligendi quos amet quam rerum'
-  },{
-    text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia enim doloremque omnis totam soluta voluptates eligendi quos amet quam rerum'
-  }];
+  const [taskText, setTaskText] = useState<string[]>([]);
+
+  const [newTaskText, setNewTaskText] = useState('');
+
+
+  function handleNewTaskText(event: FormEvent) {
+    event.preventDefault();
+    
+    setTaskText([...taskText, newTaskText]);
+    setNewTaskText('');
+    console.log(newTaskText);
+  }
+
+  function handleNewTaskTextChange(event: ChangeEvent<HTMLInputElement>) {
+    event.target.setCustomValidity('');
+
+    setNewTaskText(event.target.value);
+  }
+
+  function deleteTask(taskToDelete: string) {
+    const deletarTarefa = taskText.filter(task => {
+      return task !== taskToDelete;
+    })
+
+    setTaskText(deletarTarefa);
+  }
 
   return (
     <div>
       <section className={style.task}>
-        <input className={style.taskName} type="text" placeholder="Adicione uma nova tarefa" />
-        <button className={style.createTaskButton}>Criar <PlusCircle size={20} weight="bold" /></button>
+        <form onSubmit={handleNewTaskText}>
+          <input 
+            className={style.taskName} 
+            type="text" 
+            placeholder="Adicione uma nova tarefa" 
+            onChange={handleNewTaskTextChange} 
+          />
+
+          <button 
+            type="submit" 
+            className={style.createTaskButton}
+          >
+              Criar <PlusCircle size={20} weight="bold" />
+          </button>
+        </form>
       </section>
 
       <section className={style.taskInfo}>
         <header className={style.taskInfoHeader}>
-          <span className={style.createdTask}>Tarefas criadas <span className={style.info}>{taskNumbers}</span></span>
-          <span>Concluídas <span className={style.info}>2 de {taskNumbers}</span></span>
+          <span className={style.createdTask}>
+            Tarefas criadas 
+            <span className={style.info}>{taskText.length}</span>
+          </span>
+          <span>Concluídas <span className={style.info}>2 de {taskText.length}</span></span>
         </header>
 
-        {sum.map(item => {
-          return (
-            item ? <TaskCard text={item.text} /> : <TaskArea />
-          )
-        })}
+        {taskText.length ? taskText.map(item => <TaskCard text={item} onDeleteTask={deleteTask} />) : <TaskArea/>}
 
       </section>
     </div>
--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -2,21 +2,25 @@ import { Check, Trash } from 'phosphor-react';
 
 import style from './TaskCard.module.css';
 
-interface N {
+interface TaskProps {
   text: string;
+  onDeleteTask: (task: string) => void;
 }
-const m = '';
 
-export function TaskCard(item: N) {
+export function TaskCard({text, onDeleteTask}: TaskProps) {
+  function handleDeleteTask() {
+    onDeleteTask(text);
+  }
+  
   return (
     <div className={style.taskCard}>
       <div className={style.container} >
         <input type="checkbox" />
-        <span className={style.check}><Check/></span>
+        <span className={style.check}></span>
       </div>
 
-      <p >{item.text}</p>
-      <a> <Trash size={25} /></a>
+      <p >{text}</p>
+      <button onClick={handleDeleteTask} title='Deletar tarefa' > <Trash size={20} /></button>
     </div>
   )
 }
